f7bd57f55b03303dd806ae88e531743220f4c0a9
Manoj <hmanoj12@gmail.com>
2026-05-10 21:45:13 +0530

    Initial commit: Spring Boot Payment Application

---
+++ src/test/java/com/systemdesign/paymentapplication/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemdesign.paymentapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
